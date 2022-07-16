e29fc13a8491dbeca952af18c363def0a8a5d37a
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-16 16:37:09 +0530

    code: initial commit

---
+++ BadAtLengths/BadAtLengthsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadAtLengthsApp.swift
+//  BadAtLengths
+//
+//  Created by Admin on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BadAtLengthsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
